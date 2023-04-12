ebb000652e69d73aac672f14b85ec6982faa16eb
Pablo Muralles <pablomuralles28@gmail.com>
2023-04-12 14:46:42 -0600

    update connection

--- connection.py
+++ connection.py
@@ -2,7 +2,7 @@ import requests
 
 api_endpoint = "http://44.211.61.33:8000/"
 
-data ={
+json ={
   "nit": "66306191",
   "direction": "Ciudad",
   "date": "2023-04-11",
@@ -11,6 +11,7 @@ data ={
   "products": {"Tel1":{"Telefono":"13.54"}, "Camara":{"Camara web":"54.90"}}
 }
 
-r = requests.post(url=api_endpoint, data=data)
+r = requests.post(url=api_endpoint, json=json)
 
+print(r.status_code)
 print(r.text)
